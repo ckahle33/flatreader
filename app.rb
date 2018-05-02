bdbabca20679b78d4fcb4dd539d0bd53fc382611
cpk <ckahle33@gmail.com>
2018-05-02 03:31:05 -0500

    attempt at fixing auth bug

--- app.rb
+++ app.rb
@@ -23,13 +23,20 @@ set :environment, ENV['RACK_ENV']
 set :views, "views"
 set :database, {adapter: "postgresql", database: "flatreader_#{ENV['RACK_ENV']}"}
 
-enable :sessions
-set :sessions, :expire_after => 2419200
 
 configure :development do
+  # sessions
+  set :sessions, :expire_after => 2592000, key: 'flatreader.session'
+
+  #logging
+  enable :logging, :dump_errors, :raise_errors
   logger = Logger.new(File.open("./log/#{ENV['RACK_ENV']}.log"), "a+")
   set :logger, logger
+
+  #reloading
   enable :reloader
+
+  # errors
   use BetterErrors::Middleware
   BetterErrors.application_root = __dir__
 
@@ -39,23 +46,24 @@ end
 before do
   env["rack.errors"] = logger
   @user_sources = UserSource.where(user_id: current_user["id"]) if current_user
+  # raise 'hi'
 end
 
 get '/' do
-  # check current user
-  # need a join below
-  @articles = Article.where(source_id: user_source_ids).where.not(published_at: nil).order('published_at DESC').limit(100) # will paginate here
+  authenticate!
+    @articles = Article.where(source_id: user_source_ids).where.not(published_at: nil).order('published_at DESC').limit(100) # will paginate here
   haml :index, layout: :main
-  # create splash layout here if !current_user
 end
 
 get '/all' do
+  authenticate!
   @sources = Source.all
   # @articles = Article.all.where.not(published_at: nil).order('published_at DESC').limit(100)
   haml :all, layout: :main
 end
 
 get '/sources/:source_id' do
+  authenticate!
   @source   = Source.find(params['source_id'].to_i)
   @articles = Article.where(source_id: params['source_id']).order(published_at: :desc).limit(30)
 
@@ -63,6 +71,7 @@ get '/sources/:source_id' do
 end
 
 get '/add/:source_id' do
+  authenticate!
   @source   = Source.find(params['source_id'].to_i)
   if UserSource.find_or_create_by!(source_id: @source.id, user_id: current_user['id'])
     flash['alert-success'] = "added source!"
@@ -75,6 +84,7 @@ get '/add/:source_id' do
 end
 
 get '/remove/:user_source_id' do
+  authenticate!
   if UserSource.destroy(params["user_source_id"])
     flash['alert-success'] = "removed source!"
     redirect back
@@ -86,6 +96,7 @@ get '/remove/:user_source_id' do
 end
 
 post '/create' do
+  authenticate!
   url = URI.parse(params[:url])
   begin
     if Source.find_or_create_by!(url: url.to_s)
@@ -99,6 +110,7 @@ post '/create' do
 end
 
 post '/search' do
+  authenticate!
   query = params["q"]
 
   @sources = Source.where("name ILIKE ?", "%#{query}%")
@@ -107,6 +119,7 @@ post '/search' do
 end
 
 get '/search' do
+  authenticate!
   query = params["q"]
   @sources = Source.where("name ILIKE ?", "%#{query}%")
   haml :all, layout: :main
@@ -146,17 +159,18 @@ post '/login' do
   user = User.find_by_email(params[:email])
   if user && user.authenticate(params[:password])
     session[:user_id] = user.id
+    @current_user = user
     flash['alert-success'] = 'logged in'
     redirect '/'
   else
-    flash['alert-danger'] = "your credentials are wrong"
+    flash['alert-danger'] = "email or password was incorrect"
     redirect '/login'
   end
 end
 
 get '/logout' do
-  session[:user_id] = nil
-  redirect '/'
+  session.clear
+  redirect '/login'
 end
 
 get '/settings' do
@@ -165,6 +179,14 @@ get '/settings' do
 end
 
 helpers do
+  def authenticate!
+    if session['user_id']
+      @current_user ||= User.find(session['user_id'])
+    else
+      redirect to '/login'
+    end
+  end
+
   def base_url
     @base_url ||= "#{request.env['rack.url_scheme']}://#{request.env['HTTP_HOST']}"
   end
