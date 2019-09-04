7c27be41b9772615c1016e3514fb19abfcd44c8c
Craig Kahle <kahle@sxsw.com>
2019-09-04 12:24:33 -0500

    revert this real fast

--- public/app.js
+++ public/app.js
@@ -1,22 +1,32 @@
 import _ from 'lodash';
-import MicroModal from 'micromodal';
+import $ from 'jquery';
+import 'bootstrap';
 import 'selectize/dist/js/standalone/selectize.min.js';
 
 
-document.addEventListener("DOMContentLoaded",function(){
-  MicroModal.init({
-    onShow: modal => {
-      let button = modal.find('button')
-      modal.find('.modal-title').html(button.data("title"))
-      modal.find('.modal-body').html(
-        `
-        <a href="${button.data('url')}" target="_blank">read more</a>
-        <br>
-        ${button.data("body")}
-        `
-      )
+$(function() {
+  $('#articleModalLong').on('show.bs.modal', function(event) {
+    var button = $(event.relatedTarget)
+    var modal = $(this)
+    modal.find('.modal-title').html(button.data("title"))
+    modal.find('.modal-body').html(
+      `
+      <a href="${button.data('url')}" target="_blank">read more</a>
+      <br>
+      ${button.data("body")}
+      `
+    )
+  })
 
-    document.getElementById("source_tags").selectize({
+  var timeout = setTimeout(hideFlash, 2000)
+
+  function hideFlash() {
+    $('.flash').addClass('d-none');
+    clearTimeout(timeout);
+  }
+
+  $('#sourceModalLong').on('show.bs.modal', function(event) {
+    $("#source_tags").selectize({
       delimiter: ',',
       persist: false,
       create: function(input) {
@@ -26,13 +36,5 @@ document.addEventListener("DOMContentLoaded",function(){
           }
       }
     })
-  });
-
-  var timeout = setTimeout(hideFlash, 2000)
-
-  function hideFlash() {
-    document.querySelector('.flash').classList.add('d-none');
-    clearTimeout(timeout);
-  }
-
+  })
 })
